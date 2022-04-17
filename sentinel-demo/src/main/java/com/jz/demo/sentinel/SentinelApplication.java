eeb7ba69f8fb92b7ea0a1fa1a6dd6a5e4b0de6f0
jd <1298462930@qq.com>
2022-04-17 21:24:16 +0800

    新增 sentinel-demo 模块

---
+++ sentinel-demo/src/main/java/com/jz/demo/sentinel/SentinelApplication.java
@@ -0,0 +1,16 @@
+package com.jz.demo.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther jd
+ */
+@SpringBootApplication
+public class SentinelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SentinelApplication.class, args);
+  }
+
+}
