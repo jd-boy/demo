9feca48db9891f26981385faa7f0209e6aecba54
jd <1298462930@qq.com>
2022-04-23 14:34:16 +0800

    snetinel demo

---
+++ sentinel-demo/src/main/java/com/jz/demo/sentinel/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.jz.demo.sentinel.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther jd
+ */
+@Slf4j
+@RestController
+public class TestController {
+
+  @SentinelResource(value = "test")
+  @GetMapping(value = "/api/sentinel/test")
+  public Object get() {
+    return "test";
+  }
+
+}
