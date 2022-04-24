84f789890a8923aea2a6df16f18fcf700343e810
jd <1298462930@qq.com>
2022-04-24 21:29:14 +0800

    新增 rabbitmq-demo模块

---
+++ rabbitmq-demo/src/main/java/com/jd/demo/rabbitmq/MessageListener.java
@@ -0,0 +1,32 @@
+package com.jd.demo.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther jd
+ */
+@Slf4j
+@Component
+public class MessageListener {
+
+  @RabbitListener(bindings = @QueueBinding(
+      value = @Queue(value = "${config.rabbitmq.queue}"),
+      exchange = @Exchange(value = "${config.rabbitmq.exchange}", type = ExchangeTypes.TOPIC),
+      key = "${config.rabbitmq.routingKey}"
+  ))
+  @SneakyThrows
+  public void process(Channel channel, String msg) {
+    log.info("接收到消息：{}", msg);
+    Thread.sleep(5_000);
+    throw new RuntimeException("测试异常");
+  }
+
+}
---
+++ rabbitmq-demo/src/main/java/com/jd/demo/rabbitmq/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.jd.demo.rabbitmq.controller;
+
+import com.jd.demo.rabbitmq.MessageProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther jd
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+  private final MessageProducer messageProducer;
+
+  @Value("${config.rabbitmq.routingKey}")
+  private String routingKey;
+
+  @GetMapping(value = "/api/rabbitmq/ack-test")
+  public Object ackTest() {
+    messageProducer.send(routingKey, "测试消息");
+    return "OK";
+  }
+
+}
