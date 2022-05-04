2f6db6f83c0155c365ae0ab9fff855370d8ef927
jd <1298462930@qq.com>
2022-05-04 13:49:33 +0800

    新增springcloud-demo

---
+++ springcloud-demo/springcloud-provider/src/main/java/org/jd/demo/springcloud/provider/SpringCloudProviderApplication.java
@@ -0,0 +1,16 @@
+package org.jd.demo.springcloud.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther jd
+ */
+@SpringBootApplication
+public class SpringCloudProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudProviderApplication.class, args);
+  }
+
+}
